3c5acc7f45b6f06aaae275103be21d8fad1557b3
dancpluz <dan08jan@gmail.com>
2023-05-14 22:59:12 -0300

    Image zoom functionality

--- pages/lookbook/index.js
+++ pages/lookbook/index.js
@@ -1,47 +1,81 @@
 import styled from 'styled-components';
 import images from '../../assets/photos/index.js';
 import Image from 'next/image';
+import { useState } from 'react';
 
-const Container = styled.div`
-  
+const BigImageDiv = styled.div`
+  position: fixed;
+  display: ${props => props.showOverlay ? 'none' : 'block'};
+  background-color: black;
+  z-index: 2;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0.5;
+  cursor: pointer;
+`;
+
+const BigImage = styled(Image)`
+  z-index: 3;
+  max-height: 800px;
+  height: auto;
+  width: 800px;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  object-fit: contain;
+  opacity: 1;
 `;
 
 const Gallery = styled.div`
-
   display: flex;
-  flex-flow: row wrap;
-  margin-left: -8px; /* Adjustment for the gutter */
-  width: 100%;
+  flex-wrap: wrap;
+  margin: 80px 10vw;
+  gap: 20px;
 `;
 
 const PhotoDiv = styled.div`
-  flex: auto;
-  height: 250px;
-  min-width: 150px;
-  margin: 0 8px 8px 0;
+  height: auto;
+  flex-grow: 1;
+  flex-basis: 0;
+`;
+
+const Photo = styled(Image)`
+  width: auto;
+  height: 240px;
+  object-fit: contain;
+  cursor: pointer;
   transition: all .20s ease;
 
   &:hover {
-    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+    filter: drop-shadow(0px 7px 29px rgba(100, 100, 111, 0.2));
     transform: scale(1.05);
   }
 `;
 
-const Photo = styled(Image)`
-  width: 360px;
-  height: 100%;
-  object-fit: contain;
-`;
-
 export default function LookBook() {  
 
+  const [bigImage, setBigImage] = useState('')
+
+  function handleClick(image) {
+    setBigImage(image);
+  }
+
   return (
-    <Gallery>
-      {images.map((image) => (
-        <PhotoDiv key=''>
-          <Photo key='' src={image} />
-        </PhotoDiv>
-      ))}
-    </Gallery>
+    <>
+      <BigImageDiv onClick={() => handleClick('')} showOverlay={bigImage === ''}/>
+      <BigImage src={bigImage} onClick={() => handleClick('')} />
+        <Gallery>
+          {images.map((image) => (
+            <PhotoDiv key=''>
+              <Photo key='' src={image} onClick={() => handleClick(image)} />
+            </PhotoDiv>
+          ))}
+        </Gallery>
+    </>
   )
 }
