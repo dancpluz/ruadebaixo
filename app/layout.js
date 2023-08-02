230208f74dbdc8581541a007eb9f7879314b5bc1
dancpluz <dan08jan@gmail.com>
2023-08-02 13:35:20 -0300

    Trying to solve og metadata

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,14 @@
 import localFont from 'next/font/local';
 import StyledJsxRegistry from '../lib/registry';
 import Head from 'next/head';
+import og from '@/public/assets/og.png';
 
 const clash = localFont({ src: '../public/assets/fonts/ClashDisplay-Variable.ttf' })
 
 export const metadata = {
   title: 'Rua de Baixo',
   description: 'Streetwear, brechó, estilo',
-  image: 'assets/og.png'
+  image: og
 }
 
 export default function RootLayout({ children }) {
@@ -23,7 +24,9 @@ export default function RootLayout({ children }) {
         <meta property="og:locale" content="pt_BR" />
       </Head>
       <StyledJsxRegistry>
-          <body className={clash.className}>{children}</body>
+          <body className={clash.className}>
+            {children}
+          </body>
       </StyledJsxRegistry>
     </html>
   )
