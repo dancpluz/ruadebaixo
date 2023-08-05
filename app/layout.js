d9f1892afd25687d6da5c7a625b5efbb6471d316
dancpluz <dan08jan@gmail.com>
2023-08-04 22:02:14 -0300

    Exporting metadata working

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import localFont from 'next/font/local';
 import StyledJsxRegistry from '@/lib/registry';
-import Head from 'next/head';
 
 const clash = localFont({ src: '../public/assets/fonts/ClashDisplay-Variable.ttf' })
 
@@ -14,7 +13,7 @@ export const metadata = {
   metadataBase: new URL('https://www.ruadebaixo.com.br'),
   openGraph: {
     title: 'Rua de Baixo',
-    description: 'Onde o estilo encontra o asfalto, a Rua de Baixo aconteceTESTE',
+    description: 'Onde o estilo encontra o asfalto, a Rua de Baixo acontece',
     siteName: 'Rua de Baixo',
     url: 'https://www.ruadebaixo.com.br/',
     type: 'website',
@@ -30,20 +29,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
-      <Head>
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content={metadata.image} />
-        <meta property="og:image:width" content="800" />
-        <meta property="og:image:height" content="800" />
-        <meta property="og:type" content="website" />
-        <meta property="og:locale" content="pt_BR" />
-        <meta property="og:url" content="https://ruadebaixo.com.br" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={metadata.title} />
-        <meta name="twitter:description" content={metadata.description} />
-        <meta name="twitter:image" content={metadata.image} />
-      </Head>
       <StyledJsxRegistry>
           <body className={clash.className}>
             {children}
