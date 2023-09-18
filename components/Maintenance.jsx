26854751884a3a9f1e417092b446e90f60418243
dancpluz <dan08jan@gmail.com>
2023-09-18 14:31:17 -0300

    Maintenance text

--- components/Maintenance.jsx
+++ components/Maintenance.jsx
@@ -2,15 +2,19 @@ import { CenterScreen } from '@/components/styles/OtherStyles.styled';
 import { Logo } from '@/components/styles/LinksPage.styled';
 import logo from '@/public/assets/logonew.svg';
 import Link from 'next/link';
+import { fetchMaintenanceText } from '@/lib/config';
 
-export default function Maintenance() {
+
+export default async function Maintenance() {
+  const text = await fetchMaintenanceText()
+
   return (
     <CenterScreen>
       <Link href={'/links'}>
         <div>
           <Logo priority src={logo} alt={'Rua de Baixo Logo'} />
           <h2>Site em Manutenção</h2>
-          <p>MiniDrop Galo - 04/09</p>
+          <p>{text}</p>
         </div>
       </Link>
     </CenterScreen>
