8b6c5037bcdfe44da1c2af93796d4c25422471fa
dancpluz <dan08jan@gmail.com>
2024-10-08 23:59:35 -0300

    build: full reset

--- components/EmbedInsta.jsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { InstagramEmbed } from 'react-social-media-embed';
-
-export default function EmbedInsta({ link }) {
-  return (
-    <InstagramEmbed url={link} width={'100%'} />
-  )
-}
