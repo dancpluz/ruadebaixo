47bc9e03e96f8a40dbab67f70bffbb3ff8df1f6c
dancpluz <dan08jan@gmail.com>
2024-10-31 00:36:51 -0300

    feat: add landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,50 @@
-import { fetchFromStrapi } from "@/lib/api";
-import { Lookbook } from "@/types/api/lookbook";
+import { fetchFromStrapi } from "@/app/actions/strapi";
+import Countdown from "@/components/Countdown";
+import { buildImgUrl, selectImageUrl } from "@/lib/utils";
 import Image from 'next/image';
+import LogoAnimation from '@/components/LogoAnimation';
+import { Skeleton } from "@/components/ui/skeleton"
+import type { Home } from "@/types/api/home";
+
+export const dynamic = 'force-dynamic'
 
 export default async function Home() {
-  const data = await fetchFromStrapi<Lookbook[]>('lookbooks?populate[0]=capa&populate[1]=fotos');
-  const fotos = data?.data?.[0]?.attributes?.fotos?.data || []
-  console.log(fotos)
-  const API_URL = process.env.NEXT_PUBLIC_API_URL;
+  const data = await fetchFromStrapi<Home>('home?populate[0]=imagens');
+  const images = data.data?.attributes?.imagens?.data;
 
   return (
-    <div className="">
-      <h1 className='clash text-6xl'>Rua de Baixo</h1>
-      rdgddrghdrdhASFASFASFASFASFSA
-      {fotos.map(({id, attributes}, i) => i % 2 == 0 ? (
-        <div key={id} className='bg-accent'>
-          <Image src={API_URL + attributes.url} width={128} height={128} />
-        </div> 
-      ) : (
-        <div key={id} className=''>
-          <Image src={API_URL + attributes.url} width={128} height={128} />
-        </div>
-      ))}
-    </div>
+    <main className="mt-8 flex flex-col flex-1 gap-8 min-h-screen overflow-hidden">
+      <div>
+        <h1 className="text-4xl md:text-5xl md:text-center md:px-16 px-5 font-regular clash uppercase tracking-wider">Vendas liberadas em:</h1>
+        <h2 className="text-4xl md:text-5xl md:text-center md:px-16 px-5 font-regular clash uppercase font-semibold tracking-wider">06/11 - 18:00</h2>
+      </div>
+      <Countdown />
+      <div className="flex flex-wrap w-full -z-10 after:content-[''] after:grow-[999]">
+        {images ? images.map(({ id, attributes }) => {
+          const { formats, width, height, alternativeText } = attributes;
+
+          const { imageUrl, sizes } = selectImageUrl(formats);
+
+          return (
+            <div key={id} className="w-full outline outline-1 outline-foreground sm:w-1/2 md:w-1/3 lg:w-1/4 ">
+              <Image
+                className="object-cover h-auto w-full"
+                src={buildImgUrl(imageUrl)}
+                alt={alternativeText || `Lookbook Image ${id}`}
+                width={width}
+                height={height}
+                sizes={sizes}
+                priority={id === 0}
+              />
+            </div>
+          );
+        }) :
+          Array.from({ length: 8 }).map((_, index) => (
+            <Skeleton key={index} className="w-full h-72 rounded-lg border text-center" />
+          ))
+        }
+      </div>
+      <LogoAnimation />
+    </main>
   );
 }
