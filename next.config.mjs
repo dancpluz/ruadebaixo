676dc652161ee2fd3101ea8466f7c03309aff9ac
dancpluz <dan08jan@gmail.com>
2024-10-31 00:31:24 -0300

    build: add deps and shadcn

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,49 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  webpack(config) {
+  // Grab the existing rule that handles SVG imports
+  const fileLoaderRule = config.module.rules.find((rule) =>
+    rule.test?.test?.('.svg'),
+  )
+  config.module.rules.push(
+    // Reapply the existing rule, but only for svg imports ending in ?url
+    {
+    ...fileLoaderRule,
+    test: /\.svg$/i,
+    resourceQuery: /url/, // *.svg?url
+    },
+    // Convert all other *.svg imports to React components
+    {
+    test: /\.svg$/i,
+    issuer: fileLoaderRule.issuer,
+    resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+    use: [
+      {
+      loader: '@svgr/webpack',
+      options: {
+        icon: true,
+      },
+      },
+    ],
+    },
+  )
+  // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    fileLoaderRule.exclude = /\.svg$/i
+
+    return config
+  },
   env: {
-    SANITY_TOKEN: process.env.STRAPI_TOKEN
+    SANITY_TOKEN: process.env.STRAPI_TOKEN,
+    ASAAS_API_KEY: process.env.ASAAS_API_KEY,
+    ZAP_API_TOKEN: process.env.ZAP_API_TOKEN,
+    ZAP_URL: process.env.ZAP_URL,
+    KANGU_API_KEY: process.env.KANGU_API_KEY,
   },
   images: {
     remotePatterns: [
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,27 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
-        accent: 'hsl(var(--accent))',
-        alternate: 'hsl(var(--alternate))',
-      }
-    },
+  	extend: {
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			accent: 'hsl(var(--accent))',
+  			alternate: 'hsl(var(--alternate))'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
