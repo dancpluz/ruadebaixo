015bbe4040092bd6bf82a958d6722f3cf35ff8c3
dancpluz <dan08jan@gmail.com>
2024-11-11 00:03:04 -0300

    fix: sharp and footer

--- components/CheckoutFooter.tsx
+++ components/CheckoutFooter.tsx
@@ -2,10 +2,19 @@
 
 import { formatToBRL } from "@/lib/utils";
 import { useCart, useUser } from "@/app/Context";
+import { FormT } from "@/types/checkout";
+import { useFormContext } from "react-hook-form";
 
-export default function CheckoutFooter({ children }) {
+export default function CheckoutFooter({ children }: { children: React.ReactNode }) {
+  const form = useFormContext<FormT>();
   const { totalPrice } = useCart((state) => state)
-  const { form: { delivery, selectedDelivery, paymentType, parcels }, deliveryOptions, parcelOptions } = useUser((state) => state)
+  const { deliveryOptions, parcelOptions } = useUser((state) => state)
+
+  const delivery = form.watch('delivery')
+  const selectedDelivery = form.watch('selectedDelivery')
+  const paymentType = form.watch('paymentType')
+  const parcels = form.watch('parcels')
+
   const selectedOption = deliveryOptions.find(({ referencia }) => referencia === selectedDelivery)
   const { vlrFrete } = selectedOption || {};
 
@@ -29,7 +38,7 @@ export default function CheckoutFooter({ children }) {
         </div>
         <div className='flex uppercase text-xl text-foreground font-semibold justify-between md:text-2xl'>
           <p>Total</p>
-          <span>{paymentType === 'credit' && parcelOptions && parcels ? formatToBRL(parcelOptions.find(({ id }) => id === parcels).value) : formatToBRL(totalPrice() + (vlrFrete && delivery !== 'retirada' ? vlrFrete : 0))}</span>
+          <span>{paymentType === 'credit' && parcelOptions.length > 0 && parcels ? formatToBRL(parcelOptions.find(({ id }) => id === parcels)?.value || 0) : formatToBRL(totalPrice() + (vlrFrete && delivery !== 'retirada' ? vlrFrete : 0))}</span>
         </div>
       </div>
       <div className={'flex grow items-end justify-end gap-2'} >
