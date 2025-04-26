85d1ca0199488ebca5ed4c735984df8f24bd46e6
dancpluz <dan08jan@gmail.com>
2025-04-25 23:33:54 -0300

    feat: add xp theme styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,17 @@
 import localFont from "next/font/local";
 import "./globals.css";
-import { cn } from "@/lib/utils";
 
-const clashDisplay = localFont({
-  src: "./fonts/ClashDisplay-Variable.ttf",
-  variable: '--font-clash',
-  display: 'swap',
-});
+// const clashDisplay = localFont({
+//   src: "./fonts/ClashDisplay-Variable.ttf",
+//   variable: '--font-clash',
+//   display: 'swap',
+// });
 
-const archivo = localFont({
-  src: "./fonts/Archivo-Variable.ttf",
-  variable: '--font-archivo',
-  display: 'swap',
-});
+// const archivo = localFont({
+//   src: "./fonts/Archivo-Variable.ttf",
+//   variable: '--font-archivo',
+//   display: 'swap',
+// });
 
 export default function RootLayout({
   children,
@@ -20,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html className={clashDisplay.variable + ' ' + archivo.variable} lang="pt-BR">
+    <html lang="pt-BR">
       <body className='antialiased min-h-screen flex flex-col relative'>
         {children}
       </body>
